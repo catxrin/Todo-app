5c4c0eb59fac038ea8420e26560d5c6ff3beb267
TeodoraNS <catxrin@gmail.com>
2023-10-10 09:19:39 +0300

    added basic styles and functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,44 @@
+import TextField from "@mui/material/TextField";
+import usersData from "././../data/users.json";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
+import { useState } from "react";
 export default function Login() {
+  const [gmailUser, setGmailUser] = useState("");
+  const [userPassword, setUserPassword] = useState("");
   return (
     <div className="bg-gradient-to-br from-gray-700 via-gray-900 to-black w-screen h-screen">
-      <div className=""></div>
+      <div className="flex justify-center text-center m-auto place-items-center">
+        <div className="flex flex-col gap-5 bg-white rounded-md px-6 py-16 mt-32">
+          <Typography sx={{ fontFamily: "Gabarito" }} variant="h3">
+            Login
+          </Typography>
+          <TextField
+            onChange={(e) => setGmailUser(e.target.value)}
+            sx={{
+              width: 250,
+            }}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <TextField
+            onChange={(e) => setUserPassword(e.target.value)}
+            sx={{
+              width: 250,
+            }}
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
+          <Button
+            onClick={() => console.log(usersData.users)}
+            variant="outlined"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
