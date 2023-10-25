0ec6cd25270666ed3ff074063605064e2125747b
TeodoraNS <catxrin@gmail.com>
2023-10-25 13:23:05 +0300

    created loader and added it to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
+import { Suspense, lazy } from "react";
+import Loading from "./components/Loading";
+const RegisterPage = lazy(() => import("./pages/Register"));
+const LoginPage = lazy(() => import("./pages/Login"));
+const HomePage = lazy(() => import("./pages/Home"));
+
 function App() {
   const router = createBrowserRouter([
     {
       path: "/home",
-      element: <Home />,
+      element: (
+        <Suspense fallback={<Loading />}>
+          <HomePage />
+        </Suspense>
+      ),
     },
     {
       path: "/",
-      element: <Login />,
+      element: (
+        <Suspense fallback={<Loading />}>
+          <LoginPage />
+        </Suspense>
+      ),
     },
     {
       path: "/register",
-      element: <Register />,
+      element: (
+        <Suspense fallback={<Loading />}>
+          <RegisterPage />
+        </Suspense>
+      ),
     },
   ]);
   return <RouterProvider router={router} />;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="p-5 m-0 h-screen flex justify-center text-center items-center">
+      <p className="font-medium text-lg">🔭 Loading...</p>
+    </div>
+  );
+}
