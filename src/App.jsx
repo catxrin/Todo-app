59334559fdde44d7354fd5d5cd891e8ce41b81aa
catxrine <catxrin@gmail.com>
2023-10-30 10:40:49 +0200

    Revert "added React Final Form"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Register from "./pages/Register";
 function App() {
   const router = createBrowserRouter([
     {
@@ -11,10 +10,6 @@ function App() {
     {
       path: "/",
       element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
     },
   ]);
   return <RouterProvider router={router} />;
